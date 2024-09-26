d2dff6165328a9248590c763276e81e8d3434bbc
oanskyy <oana.anghelachi@gmail.com>
2024-09-26 17:39:32 +0300

    feat: write the smart contract template

--- contracts/BankAccount.sol
+++ contracts/BankAccount.sol
@@ -3,7 +3,29 @@ pragma solidity ^0.8.25;
 
 contract BankAccount { 
     event Deposit(); 
-    event WithdrawRequest(); 
+    event WithdrawRequested(); 
     event Withdraw(); 
-    event AccountCreated();
+    event AccountCreated(); 
+
+    struct WithdrawRequest{ address user;}
+    struct Account { 
+        address[] owners;
+    }
+
+    mapping(uint => Account) accounts; 
+    mapping(address => uint[]) userAccounts; 
+
+    uint nextAccountId; 
+    uint nextWithdrawId; 
+
+    function deposit(uint accountId) external payable {} 
+    function createAccount(address[] calldata otherOwners) external {} 
+
+    function requestWithdrawl(uint accoundId, uint amount) external {}
+    function approveWithdrawl(uint accoundId, uint withdrawId) external {}
+    function withdraw(uint accoundId, uint withdrawId) external {}
+    function getBalance(uint accoundId) public view returns (uint) {}
+    function getOwners(uint accoundId) public view returns (address[] memory) {}
+    function getApprovals(uint accoundId, uint withdrawId) public view returns (uint) {}
+    function getAccounts(uint accoundId) public view returns (uint[] memory) {}
 }
